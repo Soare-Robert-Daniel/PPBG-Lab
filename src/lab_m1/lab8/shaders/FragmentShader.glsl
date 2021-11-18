7a753a4a33bbab529d65091648212eb136b7a8da
Robert <soarerobert17@gmail.com>
2021-11-18 20:43:47 +0200

    feat: lab8 make shaders

--- src/lab_m1/lab8/shaders/FragmentShader.glsl
+++ src/lab_m1/lab8/shaders/FragmentShader.glsl
@@ -24,8 +24,12 @@ layout(location = 0) out vec4 out_color;
 void main()
 {
     // TODO(student): Define ambient, diffuse and specular light components
+    vec3 L = normalize(light_position - world_position);
+    vec3 V = normalize(eye_position - world_position);
+    vec3 H = normalize(L + V);
+
     float ambient_light = 0.25;
-    float diffuse_light = 0;
+    float diffuse_light = material_kd * max(dot(world_normal, L), 0);
     float specular_light = 0;
     // It's important to distinguish between "reflection model" and
     // "shading method". In this shader, we are experimenting with the Phong
@@ -33,17 +37,30 @@ void main()
     // Phong (1975) shading method. Don't mix them up!
     if (diffuse_light > 0)
     {
+        vec3 R = reflect(-L, world_normal);
+        specular_light = material_ks * pow(max(dot(world_normal, H), 0), material_shininess);
     }
 
     // TODO(student): If (and only if) the light is a spotlight, we need to do
     // some additional things.
+    float cut_off = radians(30.0f);
+    float spot_light = dot(-L, light_direction);
+    float spot_light_limit = cos(cut_off);
 
+    float intensitate = 0;
+    if (spot_light > cos(cut_off))
+    {
+        float linear_att = (spot_light - spot_light_limit) / (1.0f - spot_light_limit);
+        float light_att_factor = pow(linear_att, 2);
+        intensitate = material_ks + ambient_light + light_att_factor * (diffuse_light + specular_light);
+    }
+  
     // TODO(student): Compute the total light. You can just add the components
     // together, but if you're feeling extra fancy, you can add individual
     // colors to the light components. To do that, pick some vec3 colors that
     // you like, and multiply them with the respective light components.
 
     // TODO(student): Write pixel out color
-    out_color = vec4(1);
+    out_color = vec4(object_color * intensitate, 1);
 
 }
